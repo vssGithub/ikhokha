739b4d5956b31a53803b59b53c281b1a9bd809b5
vsm <vsm.upskilling@gmail.com>
2022-09-24 02:14:35 +0200

    WIP i thinketh regex finds the pattern correctly now...gets back all occurrences (11) from 3 files

--- src/com/ikhokha/techcheck/MoverMetric.java
+++ src/com/ikhokha/techcheck/MoverMetric.java
@@ -1,10 +1,16 @@
 package com.ikhokha.techcheck;
 
+import java.util.regex.Pattern;
+
 public class MoverMetric extends MetricType {
 
 	@Override
 	public void Analyze(MetricAnalyzer metricAnalyzer, String line) {
-		if (line.contains("Mover")) {
+		String regex = "Mover";
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		boolean lineContainsMatch = pattern.matcher(line).find();
+		
+		if (lineContainsMatch) {
 			metricAnalyzer.ProcessMetric(metricAnalyzer.resultsMap, "MOVER_MENTIONS");
 		}
 	}
