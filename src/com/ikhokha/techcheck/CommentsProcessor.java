496557ee8228d35688b4699077d2144674514efb
vsm <vsm.upskilling@gmail.com>
2022-09-25 19:55:59 +0200

    WIP - introduced runnable -  moved core code from FileProcessor CommentsProcessor->run (& associated methods)

--- src/com/ikhokha/techcheck/CommentsProcessor.java
+++ src/com/ikhokha/techcheck/CommentsProcessor.java
@@ -1,64 +1,40 @@
 package com.ikhokha.techcheck;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileProcessor {
+public class CommentsProcessor implements Runnable {
 	
-	private static final int NUMBER_OF_THREADS = 3;
-	static File docPath = new File("docs");
-	static File[] commentFiles = docPath.listFiles((d, n) -> n.endsWith(".txt"));
+	File[] customerCommentFiles;
+	int requiredThreads;
+	int currentThread;
+	int numberFilesPerThread;
+	int remainingFilesToAssign;
+	static File pathToFile;
 	
-	public FileProcessor() {
-	}
+	public CommentsProcessor () {}
 	
-	public static void ProcessFiles() {
-		CreateCollatedResultFile();
-		
-		Thread[] threads = new Thread[NUMBER_OF_THREADS];
-		
-		final int filesPerThread = GetFilesPerThread();
-		final int remainingFiles = GetRemainingFiles();
-		
-		// assign files dynamically to each thread
-		for (int thread = 0; thread < NUMBER_OF_THREADS; thread++) {
-			final int currentThreadIndex = thread;
-			
-			/*
-			threads[thread] = new Thread() {
-				@Override public void run() {
-					runThread(commentFiles, NUMBER_OF_THREADS, currentThreadIndex, filesPerThread, remainingFiles);
-				}
-			};
-			*/
-			
-			threads[thread] = new Thread(new CommentsProcessor(commentFiles, NUMBER_OF_THREADS, currentThreadIndex, filesPerThread, remainingFiles, docPath));
-		}
-		
-		for (Thread thread : threads) {
-			thread.start();			//start processing all thread
-		}
-		
-		for (Thread thread : threads) {
-			try {
-				thread.join();		//to wait until all threads complete execution
-			} 
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		System.out.println("processing complete");
+	public CommentsProcessor(File[] commentFiles, int numberOfThreads, int thread, int filesPerThread, int remainingFiles, File docPath) {
+		customerCommentFiles = commentFiles;
+		requiredThreads = numberOfThreads;
+		currentThread = thread;
+		numberFilesPerThread = filesPerThread;
+		remainingFilesToAssign = remainingFiles;
+		pathToFile = docPath;
 	}
 
-	private static int GetFilesPerThread() {
-		return commentFiles.length / NUMBER_OF_THREADS;
+	@Override
+	public void run() {
+		runThread(customerCommentFiles, requiredThreads, currentThread, numberFilesPerThread, remainingFilesToAssign);
 	}
 	
-	private static int GetRemainingFiles() {
-		return commentFiles.length % NUMBER_OF_THREADS;
-	}
-	/*
 	private static void runThread(File[] commentFiles, int numberOfThreads, int thread, int filesPerThread, int remainingFiles) {
 		List<File> customerComments = new ArrayList<>();
 		
@@ -66,7 +42,7 @@ public class FileProcessor {
 		AssignRemaining(commentFiles, numberOfThreads, thread, remainingFiles, customerComments);		
 		ReadFiles(customerComments);
 	}
-
+	
 	private static void SplitFiles(File[] commentFiles, int thread, int filesPerThread, List<File> customerComments) {
 		// split files between each thread
 		for (int i = thread*filesPerThread; i < (thread+1)*filesPerThread; i++) {
@@ -113,7 +89,7 @@ public class FileProcessor {
 	}
 	
 	private static BufferedWriter CreateBufferedWriter() {
-		File target = docPath.listFiles((d, n) -> n.endsWith(".log"))[0];
+		File target = pathToFile.listFiles((d, n) -> n.endsWith(".log"))[0];
 		FileWriter fw = null;
 		
 		try {
@@ -129,23 +105,6 @@ public class FileProcessor {
 	private static void CloseBufferedWriter(BufferedWriter bw) {
 		try {
 			bw.close();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	*/
-	private static void CreateCollatedResultFile() {
-		String collatedFile = docPath.getAbsolutePath();
-		
-		try {
-			File resultFile = new File(collatedFile + "\\collatedComments.log");
-			
-			if (resultFile.exists()) {
-				resultFile.delete();
-			}
-			
-			resultFile.createNewFile();
 		} 
 		catch (IOException e) {
 			e.printStackTrace();
